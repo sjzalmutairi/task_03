5f050cce1bd4c3f08dba1524cd3de636a55f1f93
Sindi <sjzalmutairi@gmail.com>
2018-07-22 22:54:06 +0300

    finished assignment

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,16 +4,30 @@ def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
-
     context = {
-
+        "my_list": [
+            {
+                "restaurant_name":"The Bowl",
+                "food_type":"Healthy",
+            },
+            {
+                "restaurant_name":"Al Makan",
+                "food_type":"Korean",
+            },
+            {
+                "restaurant_name":"Solo",
+                "food_type":"Pizza",
+            },
+        ],
     }
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
-
     context = {
-
+        "my_object": {
+            "restaurant_name":"Solo",
+            "food_type":"Pizza",
+        },
     }
     return render(request, 'detail.html', context)
